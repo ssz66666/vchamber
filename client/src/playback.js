3b8162469c414000ac74a214c25f38dee2abece7
luweijia <luweijia1013@sjtu.edu.cn>
2019-01-13 23:26:02 +0000

    polish room

--- client/src/playback.js
+++ client/src/playback.js
@@ -20,35 +20,6 @@ window.player = player;
 //     ],
 // }
 
-// Bind event listener
-function on(selector, type, callback) {
-    document.querySelector(selector).addEventListener(type, callback, false);
-}
-
-// Play
-on('.js-play', 'click', () => {
-    player.play();
-});
-
-// Pause
-on('.js-pause', 'click', () => {
-    player.pause();
-});
-
-// Stop
-on('.js-stop', 'click', () => {
-    player.stop();
-});
-
-// Rewind
-on('.js-rewind', 'click', () => {
-    player.rewind();
-});
-
-// Forward
-on('.js-forward', 'click', () => {
-    player.forward();
-});
 
 
 // Websocket Connection Logic
